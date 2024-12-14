59f5008a9a887bcd83d9fb7fddc26ade07bbf349
WazaChairmound <wazagolf@gmail.com>
2024-12-14 13:50:01 -0500

    Borrador

--- PRUEBAS/GRAFICAS/main.cpp
+++ PRUEBAS/GRAFICAS/main.cpp
@@ -2,6 +2,10 @@
 #include"mallas.h"
 #include"functmallas.h"
 #include"demosMallas.h"
+
+//#include <windows.h>
+
+
 using namespace std;
 
 // g++ -std=c++17 main.cpp mallas.cpp functmallas.cpp demosMallas.cpp -o main
@@ -11,6 +15,9 @@ int main(){
     //demoMalla1();
     //demoMalla2();
     demoMalla3();
+    Sleep(10000);
+    system("cls");
+    demoMalla3;
 
     return 0;
 }
